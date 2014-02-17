8c9f53b882d641d35376ddcc9a9e6fe36d4ffdf0
Muhammadreza <haghiri75@jabirproect.org>
2014-02-17 20:45:58 +0330

    Installation Script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp -v /usr/share/X11/xkb/symbols/us /usr/share/X11/xkb/symbols/us.bak
-sudo cp -v ./us /usr/share/X11/xkb/symbols/
+sudo cp -v /usr/share/X11/xkb/symbols/uz /usr/share/X11/xkb/symbols/uz.bak
+sudo cp -v ./uz /usr/share/X11/xkb/symbols/
 sudo rm -rvf /var/lib/xkb/*.xkm
