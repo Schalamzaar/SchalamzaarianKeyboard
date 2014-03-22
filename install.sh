d74ef9f7ee76f3691a1f9b922cd791b1f34d0939
Muhammadreza Haghiri <haghiri75@gmail.com>
2014-03-22 21:59:02 +0430

    Updates

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp -v /usr/share/X11/xkb/symbols/uz /usr/share/X11/xkb/symbols/uz.bak
-sudo cp -v ./uz /usr/share/X11/xkb/symbols/
+#sudo cp -v /usr/share/X11/xkb/symbols/uz /usr/share/X11/xkb/symbols/uz.bak
+sudo cp -v ./up /usr/share/X11/xkb/symbols/
 sudo rm -rvf /var/lib/xkb/*.xkm
